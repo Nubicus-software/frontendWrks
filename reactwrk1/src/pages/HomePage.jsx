f79c01e48e6274561c4813b67d21e3747a992bd1
Gibi paul <info@nubicus.com>
2024-07-12 16:44:19 +0530

    first commit

---
+++ reactwrk1/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='text-center'>
+          <h2 className='text-red-600'>Home page</h2>
+    </div>
+  )
+}
+
+export default HomePage
